1a0ce5c2a98dc29542c55d12aa8b4c7b95e1bace
cmbernard333@gmail.com <Christian Bernard>
2024-06-17 20:30:32 -0700

    [WIP] dsdhacked
    TODO:
    
    - [X] Dynamically allocated sprite names
    - [X] Dynamically allocated mobjinfo
    - [X] Dynamically allocated state_t
    - [ ] Odamex states appended to the end of the dynamically allocated
      states
    - [X] Zone memory allocation for char* sprite names
    - [ ] Zone memory allocation for state_t
    - [ ] Zone memory allocation for mobjinfo objects (???)
    - [ ] Dehacked Undo with dynamically allocated doom object collections
    - [ ] Update use of NUMSPRITES where applicable
    - [ ] Update use of NUMSTATES where applicable
    - [ ] Update use of NUMMOBJTYPES where applicable
    
    * Added dynamically allocated array for mobjinfo types
      * Includes new global variable num_mobjinfo_types and
        D_Initialize_mobjinfo function
      * These are managed by new functions defined in
        mobjinfo.h/mobjinfo.cpp
      * mobjinfo is initialized pointing at doom_mobjinfo. This saves an
        allocation and is safer when realloc-ing and replacing.
    * Added dynamically allocated array for states
      * Includes a new global variable num_state_t_types and
        D_Initialize_states function
      * These are managed by new functions defined in state.h/state.cpp
      * states is not initialized from boomstates and odastates
    * Added dynamically allocated array for sprite names
      * Includes new global variable num_spritet_types and
        D_Initialize_sprnames function
      * These are managed by new functions defined in sprite.h/sprite.cpp
      * sprnames is initialized pointing to doom_sprnames. This saves an
        allocation and is safer when realloc'ing and replacing.
    * Base mobinfo types max is previous limit
    * Base sprite types max is previous limit
    * Base state types max is previous limit

--- common/p_pspr.cpp
+++ common/p_pspr.cpp
@@ -185,7 +185,7 @@ void P_SetPspritePtr(player_t* player, pspdef_t* psp, statenum_t stnum)
 			break;
 		}
 
-		if (stnum >= NUMSTATES)
+		if (stnum >= ::NUMSTATES)
 			return;
 
 		psp->state = &states[stnum];
