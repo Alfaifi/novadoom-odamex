9639364869dc672195da78c4cfb8ede30586a4a1
cmbernard333@gmail.com <Christian Bernard>
2024-09-16 08:57:26 -0700

    Fix dehacked backup doing a double free after converting backupsprnames to doom_obj_container.

--- common/info.h
+++ common/info.h
@@ -239,7 +239,7 @@ inline auto format_as(spritenum_t eSpriteNum)
 
 // [CMD] TODO: new types and function to allocate sprnames for dsdhacked
 extern const char* doom_sprnames[];
-extern DoomObjectContainer<char*, spritenum_t> sprnames;
+extern DoomObjectContainer<const char*, spritenum_t> sprnames;
 extern size_t num_spritenum_t_types();
 
 inline FArchive &operator<< (FArchive &arc, spritenum_t i) { DWORD out; out = i; return arc << out; }
