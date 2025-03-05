0d839e3cdcc39ba6116cc2a4857f3bb38ee81c43
Christian Bernard <cmbernard333@gmail.com>
2025-03-05 14:28:08 -0800

    Create odahome if it doesn't exist.

--- docker/examples/run.sh
+++ docker/examples/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+if [[ ! -d ./odahome ]]; then
+    mkdir odahome
+fi
 UP_DOWN=${1:-up -d}
 DOCKER_COMPOSE=$(which docker-compose 2>/dev/null)
 if [[ -z "$DOCKER_COMPOSE" ]]; then
