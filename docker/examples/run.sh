84b4d49b82b0f7a545a36ff88e49dd7221693d16
Christian Bernard <cmbernard333@gmail.com>
2025-03-05 13:41:04 -0800

    Update run.sh to allow bringing the container up and down via the first parameter

--- docker/examples/run.sh
+++ docker/examples/run.sh
@@ -1,1 +1,7 @@
-docker-compose -p odamex up -d
+#!/usr/bin/env bash
+UP_DOWN=${1:-up -d}
+DOCKER_COMPOSE=$(which docker-compose 2>/dev/null)
+if [[ -z "$DOCKER_COMPOSE" ]]; then
+    DOCKER_COMPOSE="docker compose"
+fi
+$DOCKER_COMPOSE -p odamex ${UP_DOWN}
