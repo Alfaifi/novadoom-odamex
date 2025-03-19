0c6e19b714baa48926995fc1787d92ce558ed3b2
Christian Bernard <cmbernard333@gmail.com>
2025-03-19 16:51:33 -0700

    New fix for calculating the correct psprnum for the action being called. This one works with eviternity 2 and doosk.

--- common/p_pspr.cpp
+++ common/p_pspr.cpp
@@ -22,7 +22,6 @@
 //
 //-----------------------------------------------------------------------------
 
-
 #include "odamex.h"
 
 #include <stdlib.h>
@@ -203,7 +202,11 @@ void P_SetPspritePtr(player_t* player, pspdef_t* psp, int32_t stnum)
 		if (psp->state->action)
 		{
 			if (!player->spectator && player->mo != NULL)
+			{
+				// [CMB] TODO: can we calculate psprnum here using the present psp and length of psprites?
+				player->psprnum = psp - player->psprites;
 				psp->state->action(player->mo);
+			}
 
 			if (!psp->state)
 				break;
@@ -218,7 +221,6 @@ void P_SetPspritePtr(player_t* player, pspdef_t* psp, int32_t stnum)
 void P_SetPsprite(player_t* player, int position, int32_t stnum)
 {
 	// TODO [CMB] set psprnum here to the correct position in the sprites
-	player->psprnum = position;
 	P_SetPspritePtr(player, &player->psprites[position], stnum);
 }
 
