d185605a130cbfb57fc848e9e8ebd9d476431e92
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-04-09 17:50:28 -0500

    Build libdwarf and zstd externally from cpptrace

--- libraries/cpptrace-lib.cmake
+++ libraries/cpptrace-lib.cmake
@@ -1,7 +1,7 @@
 ### cpptrace ###
 
 if(BUILD_CLIENT OR BUILD_SERVER)
-  lib_buildgen(LIBRARY cpptrace PARAMS "-DCMAKE_DEBUG_POSTFIX=d")
+  lib_buildgen(LIBRARY cpptrace PARAMS "-DCMAKE_DEBUG_POSTFIX=d -DCPPTRACE_USE_EXTERNAL_LIBDWARF=On")
   lib_build(LIBRARY cpptrace)
 
   find_package(cpptrace)
