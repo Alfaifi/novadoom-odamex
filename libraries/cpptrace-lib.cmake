c7a4b4867282bb4936b3a7e33afb779c9e886da1
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-04-10 00:10:50 -0500

    Fix building without --share=network

--- libraries/cpptrace-lib.cmake
+++ libraries/cpptrace-lib.cmake
@@ -1,7 +1,10 @@
 ### cpptrace ###
 
 if(BUILD_CLIENT OR BUILD_SERVER)
-  lib_buildgen(LIBRARY cpptrace PARAMS "-DCMAKE_DEBUG_POSTFIX=d -DCPPTRACE_USE_EXTERNAL_LIBDWARF=On")
+  lib_buildgen(
+    LIBRARY cpptrace
+    PARAMS "-DCMAKE_DEBUG_POSTFIX=d"
+           "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=${USE_EXTERNAL_LIBDWARF}")
   lib_build(LIBRARY cpptrace)
 
   find_package(cpptrace)
