edafe013be366059f90fa16a63b3e5f6307cc408
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-04-15 00:07:32 -0500

    Rewrite ParseString to not use com_token

--- common/c_dispatch.h
+++ common/c_dispatch.h
@@ -25,6 +25,7 @@
 
 #include "dobject.h"
 
+#include <optional>
 
 void C_ExecCmdLineParams (bool onlyset, bool onlylogfile);
 
@@ -33,7 +34,7 @@ void C_ExecCmdLineParams (bool onlyset, bool onlylogfile);
 void AddCommandString(const std::string &cmd, uint32_t key = 0);
 
 // parse a command string
-const char *ParseString (const char *data);
+std::optional<std::string> ParseString (std::string_view& data);
 
 // combine many arguments into one valid argument.
 std::string C_ArgCombine(size_t argc, const char **argv);
@@ -65,7 +66,7 @@ protected:
 	char **argv;
 	char *args;
 
-	friend void C_DoCommand(const char *cmd, uint32_t key);
+	friend void C_DoCommand(std::string_view cmd, uint32_t key);
 };
 
 #define BEGIN_COMMAND(n) \
