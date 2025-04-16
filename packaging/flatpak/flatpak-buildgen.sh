441755fc99dcc721d86c6e5c8e81e703d80e6cd8
Mia McMahill <miadawnmc@gmail.com>
2025-04-15 19:39:28 -0500

    Clean up build script

--- packaging/flatpak/flatpak-buildgen.sh
+++ packaging/flatpak/flatpak-buildgen.sh
@@ -52,7 +52,7 @@ cp -r packaging/linux/$projectId.metainfo.xml $metadataDir/
 # Install the odd demo filetype
 iconDir=/app/share/icons/hicolor/512x512/mimetypes
 mkdir -p $iconDir
-cp -r ./media/icon_odademo_512.png $iconDir/$projectId-application-odamex-demo.png
+cp -r media/icon_odademo_512.png $iconDir/$projectId-application-odamex-demo.png
 mimeDir=/app/share/mime/packages/
 mkdir -p $mimeDir
 cp -r packaging/linux/$projectId-mime.xml $mimeDir/$projectId-mime.xml
@@ -61,72 +61,42 @@ cp -r packaging/linux/$projectId-mime.xml $mimeDir/$projectId-mime.xml
 mkdir -p /app/bin
 cp -r build/wad/odamex.wad /app/bin/
 
+# Install the executables
+install_executable() {
+    local projectName=$1
+    local projectId=$2
+    local executableName=$3
+    local buildFolder=$4
+
+    # Copy the app to the Flatpak-based location.
+    mkdir -p /app/$projectName
+    install -c build/$buildFolder/$executableName /app/$projectName/$executableName
+    ln -s /app/$projectName/$executableName /app/bin/$executableName
+
+    # Install the icon.
+    iconDir=/app/share/icons/hicolor/512x512/apps
+    mkdir -p $iconDir
+    cp -r media/icon_${executableName}_512.png $iconDir/$projectId.png
+
+    # Install the desktop file.
+    desktopFileDir=/app/share/applications
+    mkdir -p $desktopFileDir
+    cp -r packaging/linux/$projectId.desktop $desktopFileDir/
+}
+
 # Client app
-projectName=Odamex.Client
-projectId=net.odamex.Odamex.Client
-executableName=odamex
-
-# Copy the client app to the Flatpak-based location.
-mkdir -p /app/$projectName
-install -c build/client/$executableName /app/$projectName/$executableName
-ln -s /app/bin/odamex.wad /app/$projectName/odamex.wad
-ln -s /app/$projectName/$executableName /app/bin/$executableName
-
-# Install the icon.
-iconDir=/app/share/icons/hicolor/512x512/apps
-mkdir -p $iconDir
-cp -r ./media/icon_odamex_512.png $iconDir/$projectId.png
-
-# Install the desktop file.
-desktopFileDir=/app/share/applications
-mkdir -p $desktopFileDir
-cp -r packaging/linux/$projectId.desktop $desktopFileDir/
+install_executable Odamex.Client net.odamex.Odamex.Client odamex client
+ln -s /app/bin/odamex.wad /app/Odamex.Client/odamex.wad
 
 # Server app
-projectName=Odamex.Server
-projectId=net.odamex.Odamex.Server
-executableName=odasrv
+install_executable Odamex.Server net.odamex.Odamex.Server odasrv server
+ln -s /app/bin/odamex.wad /app/Odamex.Server/odamex.wad
 
-# Copy the server app to the Flatpak-based location.
-mkdir -p /app/$projectName
-install -c build/server/$executableName /app/$projectName/$executableName
-mkdir -p /app/bin
-ln -s /app/bin/odamex.wad /app/$projectName/odamex.wad
-ln -s /app/$projectName/$executableName /app/bin/$executableName
+# Launcher app
+install_executable Odamex.Launcher net.odamex.Odamex.Launcher odalaunch odalaunch
 
 # Copy config samples into the flatpak
 cp -r config-samples /app/$projectName/
-
-# Install the icon.
-iconDir=/app/share/icons/hicolor/512x512/apps
-mkdir -p $iconDir
-cp -r ./media/icon_odasrv_512.png $iconDir/$projectId.png
-
-# Install the desktop file.
-desktopFileDir=/app/share/applications
-mkdir -p $desktopFileDir
-cp -r packaging/linux/$projectId.desktop $desktopFileDir/
-
-# Launcher app
-projectName=Odamex.Launcher
-projectId=net.odamex.Odamex.Launcher
-executableName=odalaunch
-
-# Copy the launcher app to the Flatpak-based location.
-mkdir -p /app/$projectName
-install -c build/odalaunch/$executableName /app/$projectName/$executableName
-mkdir -p /app/bin
-ln -s /app/$projectName/$executableName /app/bin/$executableName
-
-# Install the icon.
-iconDir=/app/share/icons/hicolor/512x512/apps
-mkdir -p $iconDir
-cp -r ./media/icon_odalaunch_512.png $iconDir/$projectId.png
-
-# Install the desktop file.
-desktopFileDir=/app/share/applications
-mkdir -p $desktopFileDir
-cp -r packaging/linux/$projectId.desktop $desktopFileDir/
 
 # Install helper script
 install -c packaging/flatpak/select-exe.sh /app/bin/select-exe
