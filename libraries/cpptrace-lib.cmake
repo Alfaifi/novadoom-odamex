9a29a7dc81d359b59b869c074701315c07db62ac
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-08-15 16:47:29 -0500

    Cache zlib include directory

--- libraries/cpptrace-lib.cmake
+++ libraries/cpptrace-lib.cmake
@@ -31,6 +31,8 @@ if(BUILD_CLIENT OR BUILD_SERVER)
         set(ZLIB_LIBRARY
           "${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}z${libsuffix}")
       endif()
+      set(ZLIB_INCLUDE_DIR
+        "${CMAKE_CURRENT_BINARY_DIR}/local/include" CACHE PATH "" FORCE)
     endif()
 
     lib_buildgen(
