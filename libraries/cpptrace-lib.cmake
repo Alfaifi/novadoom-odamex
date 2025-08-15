4c731e77e79bde2cd34b5ecc49e40bf1ba6f463a
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-08-15 17:52:19 -0500

    Cache library too

--- libraries/cpptrace-lib.cmake
+++ libraries/cpptrace-lib.cmake
@@ -2,43 +2,24 @@
 
 if(BUILD_CLIENT OR BUILD_SERVER)
   if(USE_INTERNAL_CPPTRACE)
-      set(_CPPTRACE_BUILDGEN_PARAMS
-        "-DCMAKE_DEBUG_POSTFIX=d"
-        "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=${USE_EXTERNAL_LIBDWARF}")
-
-
-    if(USE_INTERNAL_ZLIB)
-      list(APPEND _CPPTRACE_BUILDGEN_PARAMS
-        "-DZLIB_ROOT=${CMAKE_CURRENT_BINARY_DIR}/local")
-      list(APPEND _CPPTRACE_BUILDGEN_PARAMS
-        "-DZLIB_INCLUDE_DIR=${CMAKE_CURRENT_BINARY_DIR}/local/include")
-      if (WIN32)
-        list(APPEND _CPPTRACE_BUILDGEN_PARAMS
-          "-DZLIB_LIBRARY=${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}libzstatic${libsuffix}")
-      else()
-        list(APPEND _CPPTRACE_BUILDGEN_PARAMS
-          "-DZLIB_LIBRARY=${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}z${libsuffix}")
-      endif()
-      # Set vars so the finder can find them.
-      set(ZLIB_INCLUDE_DIR
-        "${CMAKE_CURRENT_BINARY_DIR}/local/include")
-      set(ZLIB_ROOT
-        "${CMAKE_CURRENT_BINARY_DIR}/local")
-      if(WIN32)
-        set(ZLIB_LIBRARY
-          "${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}zlibstatic${libsuffix}")
-      else()
-        set(ZLIB_LIBRARY
-          "${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}z${libsuffix}")
-      endif()
-      set(ZLIB_INCLUDE_DIR
-        "${CMAKE_CURRENT_BINARY_DIR}/local/include" CACHE PATH "" FORCE)
-    endif()
-
     lib_buildgen(
       LIBRARY cpptrace
-      PARAMS ${_CPPTRACE_BUILDGEN_PARAMS})
+      PARAMS "-DCMAKE_DEBUG_POSTFIX=d"
+             "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=${USE_EXTERNAL_LIBDWARF}")
     lib_build(LIBRARY cpptrace)
+  endif()
+
+  if(USE_INTERNAL_ZLIB)
+    # Set vars so the finder can find them.
+    set(ZLIB_INCLUDE_DIR
+    "${CMAKE_CURRENT_BINARY_DIR}/local/include" CACHE PATH "" FORCE)
+    if(WIN32)
+      set(ZLIB_LIBRARY
+        "${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}zlibstatic${libsuffix}"  CACHE PATH "" FORCE)
+    else()
+      set(ZLIB_LIBRARY
+        "${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}z${libsuffix}" CACHE PATH "" FORCE)
+    endif()
   endif()
 
   find_package(cpptrace)
