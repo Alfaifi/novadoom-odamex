262813fe06f63322a7aaf89ec553c50ba5e1b6b2
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-08-15 14:24:59 -0500

    Allow linking against external cpptrace, and when building cpptrace with USE_INTERAL_ZLIB, point it at the internal zlib

--- libraries/cpptrace-lib.cmake
+++ libraries/cpptrace-lib.cmake
@@ -1,11 +1,24 @@
 ### cpptrace ###
 
 if(BUILD_CLIENT OR BUILD_SERVER)
-  lib_buildgen(
-    LIBRARY cpptrace
-    PARAMS "-DCMAKE_DEBUG_POSTFIX=d"
-           "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=${USE_EXTERNAL_LIBDWARF}")
-  lib_build(LIBRARY cpptrace)
+  if(USE_INTERNAL_CPPTRACE)
+      set(_CPPTRACE_BUILDGEN_PARAMS
+        "-DCMAKE_DEBUG_POSTFIX=d"
+        "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=${USE_EXTERNAL_LIBDWARF}")
+
+
+    if(USE_INTERNAL_ZLIB)
+      list(APPEND _CPPTRACE_BUILDGEN_PARAMS
+        "-DZLIB_INCLUDE_DIR=${CMAKE_CURRENT_BINARY_DIR}/local/include")
+      list(APPEND _CPPTRACE_BUILDGEN_PARAMS
+        "-DZLIB_LIBRARY=${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}zlibstatic${libsuffix}")
+    endif()
+
+    lib_buildgen(
+      LIBRARY cpptrace
+      PARAMS ${_CPPTRACE_BUILDGEN_PARAMS})
+    lib_build(LIBRARY cpptrace)
+  endif()
 
   find_package(cpptrace)
   if(TARGET cpptrace::cpptrace)
