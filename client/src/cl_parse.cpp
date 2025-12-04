8324660d6ca1c49269d790de32876ebf40b72d8f
Mia McMahill <miadawnmc@gmail.com>
2025-12-03 23:48:43 -0600

    Fix broken format string for serverchat

--- client/src/cl_parse.cpp
+++ client/src/cl_parse.cpp
@@ -1443,7 +1443,7 @@ static void CL_Print(const odaproto::svc::Print* msg)
 	else if (level == PRINT_TEAMCHAT)
 		PrintFmt(level, "{:c}!{}", TEXTCOLOR_ESCAPE, str);
 	else if (level == PRINT_SERVERCHAT)
-		PrintFmt(level, "{}", TEXTCOLOR_YELLOW, str);
+		PrintFmt(level, "{:c}{}", TEXTCOLOR_YELLOW, str);
 	else
 		PrintFmt(level, "{}", str);
 
